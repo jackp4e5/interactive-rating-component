d62d0db9f79592d34fa1099b87d02c0cd309981f
jack pachon <jack.kilian0196@gmail.com>
2022-09-12 21:29:15 -0500

    changes in first card and second card components

--- build/js/app.js
+++ build/js/app.js
@@ -1,12 +1,22 @@
 
 const buttons = document.querySelectorAll('.options__item');
-const form = document.querySelector('#form');
 const selected = document.querySelector('.selected__option');
 const front = document.querySelector('#front');
 const back = document.querySelector('#back');
+const form = document.querySelector('#form');
 
 document.addEventListener('DOMContentLoaded',putData)
 form.addEventListener('submit',change);
+
+function putData(){
+
+    buttons.forEach(items =>{
+
+        items.addEventListener('click', ()=>{
+            selected.textContent = items.textContent;
+        });
+    });
+ }
 
 function change(e){
     e.preventDefault();
@@ -16,18 +26,10 @@ function change(e){
     setTimeout(() => {
         front.classList.remove('front--active');
         back.classList.remove('back--active'); 
-    }, 4000);
+    }, 6100);
 }
 
- function putData(){
 
-    buttons.forEach(items =>{
-
-        items.addEventListener('click', ()=>{
-            selected.textContent = items.textContent;
-        });
-    });
- }
 
 
 
